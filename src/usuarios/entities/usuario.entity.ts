af8560c94d7256fc95c61ee395d597d3ad826633
Alan Ramalho <ramalho.sit@gmail.com>
2023-02-13 20:50:02 -0300

    entity

--- src/usuarios/entities/usuario.entity.ts
+++ src/usuarios/entities/usuario.entity.ts
@@ -1,8 +1,10 @@
-export class Usuario {
+import { Usuarios } from '@prisma/client';
+
+export class UsuarioEntity implements Usuarios {
   CodUso: number;
   Email: string;
   Nome: string;
   SobreNome: string;
-  CreatedAt?: Date;
-  UpdatedAt?: Date | null;
+  CreatedAt: Date;
+  UpdatedAt: Date;
 }
